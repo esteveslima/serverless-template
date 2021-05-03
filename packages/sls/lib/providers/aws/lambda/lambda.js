eda2fbb3778047932956661d90f4f61862df0288
Esteves <esteveslima@ESTEVES-UBUNTU>
2021-05-02 23:33:05 -0300

    test - lerna common package changes propagation

--- packages/sls/lib/providers/aws/lambda/lambda.js
+++ packages/sls/lib/providers/aws/lambda/lambda.js
@@ -10,7 +10,7 @@ export default (func) => async function lambda() {
   // Register default middlewares
   middleware.before((args) => { if (!IS_OFFLINE) logger.info(args); }); // TODO: enhance logs to beautify view on cloudwatch
   middleware.after((args, result) => { if (!IS_OFFLINE) logger.info(result); });
-  middleware.error((args, errorObject) => { if (!IS_OFFLINE) logger.error('Custom error middleware'); });
+  middleware.error((args, errorObject) => { if (!IS_OFFLINE) logger.error('Custom middleware for errors'); });
 
   // Run function(between middlewares)
   const result = await middleware.resolve(func, arguments);
