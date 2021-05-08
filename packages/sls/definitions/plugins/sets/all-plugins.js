168730dd9ed3a577cddbb016164075f4fb6254a5
Esteves <esteveslima@ESTEVES-UBUNTU>
2021-05-08 00:31:06 -0300

    chore: localstack initial basic working example(disabled)
    localstack container with its serverless plugin configurations, not working properly

--- packages/sls/definitions/plugins/sets/all-plugins.js
+++ packages/sls/definitions/plugins/sets/all-plugins.js
@@ -8,11 +8,33 @@ const serviceTempDir = '.temp/'; // relative path to temporary folder used for e
 // The paths references in properties are from the project folder scope(not the monorepo root folder), because this file will be imported and used as boilerplate
 // List of every plugin with its custom configuration(the items order may be relevant)
 module.exports = {
-  // (must be the first on the list)bundle functions code with tree shaking, transcompiling with babel
+
+  // // EXPERIMENTAL: aws local services
+  // // some features not working(see localstack-container logs to find out which ones to disable, e.g: httpApi)
+  // // access the function adding the /route... after the provided endpoint
+  // // requires to reload container in each deploy
+  // // may require to reinstall dependencies at root dir after restarting container
+  // 'serverless-localstack': {
+  //   localstack: {
+  //     host: 'http://localstack-container',
+  //     edgePort: '4566',
+  //     stages: ['local'],
+  //     // lambda: {
+  //     //   mountCode: true,
+  //     // },
+  //     docker: {
+  //       sudo: true,
+  //     },
+  //     autoStart: false,
+  //   },
+  // },
+
+  // bundle functions code with tree shaking, transcompiling with babel(must come before than offline servers)
   'serverless-webpack': {
     webpack: {
       webpackConfig: './webpack.config.js',
       includeModules: { forceExclude: ['aws-sdk'] },
+      keepoutputDirectory: true, // required for localstack
     },
   },
 
